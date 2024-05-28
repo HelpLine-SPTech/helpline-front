a8b0f35ef023ffe4ddb975750988a0b399df2d12
NAYRA BELARMINO <nayra.belarmino@atos.net>
2024-05-28 17:35:29 -0300

    Atualização footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,41 +8,39 @@ import './footer.css'
 function Footer() {
   return (
     <>
-    <div id='container'>
-      <div className='footer-wrapper'>
-        <div className='logo-container'>
-          <img src={LogoBranca} alt='Logo branca' />
-        </div>
-        <div className='columns'>
-          <div className='column'>
-            <span>ONGs</span>
-            <span>Voluntariado</span>
-            <span>Causas</span>
-            <span>Fórum</span>
+      <div id='container'>
+        <div className='waves-container-img'><img src={Waves} alt='waves footer' className='waves-img' /></div>
+        <div className='footer-wrapper'>
+          <div className='logo-container'>
+            <img src={LogoBranca} alt='Logo branca' />
           </div>
-          <div className='column'>
-            <span>Cadastre-se</span>
-            <span>Login</span>
-          </div>
-          <div className='column'>
-            <span><b>Contrate-nos</b></span>
-            <span>helpline@gmail.com</span>
-            <div className='social-links'>
-              <span><b>Redes Sociais</b></span>
-              <div className='icons' >
-                <div className='icones-redes'>
-                <img src={Instagram} alt='Logo Instagram' />
-                <img src={Linkedin} alt='Logo LinkedIn' />
+          <div className='columns'>
+            <div className='column'>
+              <span>ONGs</span>
+              <span>Voluntariado</span>
+              <span>Causas</span>
+              <span>Fórum</span>
+            </div>
+            <div className='column'>
+              <span>Cadastre-se</span>
+              <span>Login</span>
+            </div>
+            <div className='column'>
+              <span><b>Contrate-nos</b></span>
+              <span>helpline@gmail.com</span>
+              <div className='social-links'>
+                <span><b>Redes Sociais</b></span>
+                <div className='icons' >
+                  <div className='icones-redes'>
+                    <img src={Instagram} alt='Logo Instagram' />
+                    <img src={Linkedin} alt='Logo LinkedIn' />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div >
-        <img src={Waves} alt='waves footer'className='waves-container' />
-      </div>
-    </div>
     </>
   )
 }
