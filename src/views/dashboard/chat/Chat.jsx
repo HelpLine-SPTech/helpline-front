b6b2e5436100555c9ca928b3173b698bb7e631e7
GuiScarabelli <guilherme.quadros@sptech.school>
2024-06-11 21:03:35 -0300

    Adicionando função de pesquisa

--- src/views/dashboard/chat/Chat.jsx
+++ src/views/dashboard/chat/Chat.jsx
@@ -11,7 +11,6 @@ import {
 import DefaultProfile from "../../../assets/defaultProfilePic.svg";
 import sendInput from "../../../assets/sendInput.svg";
 import "./Chat.css";
-import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
 
 export const ChatContext = React.createContext();
 
@@ -20,13 +19,14 @@ function Chat() {
   const chatInputRef = useRef();
 
   const chatService = ChatService.instance;
+  const [search, setSearch] = useState('')
+
   const [selectedUser, setSelectedUser] = useState("");
   const [selectedUserName, setSelectedUserName] = useState("");
   const [selectedUserProfilePic, setSelectedUserProfilePic] = useState("");
 
   const user = useSelector(selectUser);
   const getMessages = useSelector(selectMessages);
-  const notifications = useSelector(selectNotifications);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
@@ -34,10 +34,18 @@ function Chat() {
     chatInputRef.current.value = "";
   }
 
+  function onSearchChange(e) {
+    setSearch(e.target.value)
+  }
+
   useEffect(() => {
     setMessages(getMessages);
+    if(selectedUser){
+      chatService.fetchMessages(selectedUser);
+    }
     const element = chatContainerRef.current;
     element && element.scrollTo(0, element.scrollHeight);
+
   }, [selectedUser, messages, getMessages]);
 
   return (
@@ -50,60 +58,82 @@ function Chat() {
           setSelectedUserProfilePic,
         }}
       >
-
         <div className="bg-green d-flex">
           <DashboardSideBar />
-          <div
-            className="dash-content d-flex"
-            style={{ flexDirection: "column" }}
-          >
-            <div className="header-chat">
+          <div className="dash-content d-flex">
+            <div className="sidebar-chat">
               <div className="pesquisa-chat">
                 <input
                   type="text"
                   placeholder="Pesquisar"
                   className="inp-pesquisar pd-h-16"
+                  onChange={onSearchChange}
                 />
               </div>
-              {selectedUser && (
-                <div className="info-chat">
-                  <img className="foto-doador" src={`${selectedUserProfilePic ?  selectedUserProfilePic : DefaultProfile}`} alt="Foto do doador" />
-                  <div className="nome-doador">
-                    {selectedUserName}
-                  </div>
-                </div>                
-              )}
+              <AbaConversa search={search} />
             </div>
-            <div className="chat-body">
-              <AbaConversa />
-              {(selectedUser) &&( 
-                <div  style={{ width: "100%" }}>
-                <div className="chat-content-container" ref={chatContainerRef}>
-                <div className="chat-content" id="chat-messages">
-                  {messages.map((m, i) => {
-                    return(
-                    <div key={i} className="message">
-                      <div className={`message-content font-poppins ${m.senderId === user.id ? 'sender' : 'receiver' }`} >
-                        <div className="message-text">{m.content || m}</div>
-                        <div className="message-time">{chatService.getHours(m.timestamp)}</div>
-                      </div>  
-                    </div>  
-                )})}
-                </div>
+
+            <div className="chat-content">
+              <div className="chat-header">
+                {selectedUser && (
+                  <div className="info-chat">
+                    <img
+                      className="foto-doador"
+                      src={`${
+                        selectedUserProfilePic
+                          ? selectedUserProfilePic
+                          : DefaultProfile
+                      }`}
+                      alt="Foto do doador"
+                    />
+                    <div className="nome-doador">{selectedUserName}</div>
+                  </div>
+                )}
               </div>
-              <div className="input-message">
+
+              <div className="chat-messages" ref={chatContainerRef}>
+                {selectedUser && (
+                  <div>
+                    {messages.map((m, i) => {
+                      return (
+                        <div key={i} className="message">
+                          <div
+                            className={`message-content font-poppins ${
+                              m.senderId === user.id ? "sender" : "receiver"
+                            }`}
+                          >
+                            <div className="message-text">{m.content || m}</div>
+                            <div className="message-time">
+                              {chatService.getHours(m.timestamp)}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+              <div style={{ backgroundColor: "#E1E1E1", display: "flex" }}>
                 <input
                   type="text"
+                  className="input-message"
                   id="message"
                   onChange={(e) => setMessage(e.target.value)}
                   placeholder="Digite sua mensagem..."
                   ref={chatInputRef}
                 />
-                      
-                <button className = 'send-message-button' onClick={(e) => { e.preventDefault(); chatService.sendMessage(message, selectedUser); clearInput()}}>Enviar mensagem</button>
-                </div>
-             </div>
-                )}
+
+                <button
+                  className="send-message-button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    chatService.sendMessage(message, selectedUser);
+                    clearInput();
+                  }}
+                >
+                  <i className="bi bi-send icon-g icon-white"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
