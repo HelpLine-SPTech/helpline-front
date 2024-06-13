d919bbd614e358a5762d284905979b8a70b6357c
NAYRA BELARMINO <nayra.belarmino@atos.net>
2024-06-13 16:25:53 -0300

    Ajuste das telas

--- src/components/Forum/PerfilUsuario/PerfilUsuario.jsx
+++ src/components/Forum/PerfilUsuario/PerfilUsuario.jsx
@@ -11,14 +11,14 @@ function PerfilUsuario() {
                         <img className='moldura-foto' src={imgPessoa} alt="" />
                     </div>
                     <div className='bio-usuario'>
-                        <h4>Isabella Almeida</h4>
+                        <h2>Isabella Almeida</h2>
                         <p>Sou voluntária há 5 anos, desde então tenho dedicado meu tempo e energia para causas sociais que acredito. Sou apaixonada por tudo  isso!</p>
                     </div>
                     <button className='button-perfil'>Mensagens diretas</button>
                     <button className='button-perfil'>Minhas Postagens</button>
                 </div>
                 <div class="notification-box">
-                    <h4>Notificações</h4>
+                    <h2>Notificações</h2>
                     <span className='sub-title'>Campanhas em destaque</span>
                     <div class="notification-item">
                         <span class="dot"></span>
@@ -39,7 +39,7 @@ function PerfilUsuario() {
                     </div>
                 </div>
                 <div class="notification-box">
-                    <h4>Vagas</h4>
+                    <h2>Vagas</h2>
                     <span className='sub-title'>Vagas em destaque</span>
                     <div class="notification-item">
                         <span class="dot"></span>
--- src/views/institucional/TelaOng.jsx
+++ src/views/institucional/TelaOng.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Footer } from "../../components";
 import { Link } from 'react-router-dom'
-import  {NavInstitucional}  from "../../components";
+import { NavInstitucional } from "../../components";
 import GroupSelo from "../../assets/GroupSelo.svg";
 import setaEsquerda from "../../assets/CirculeSetaEsquerda.svg";
 import setaDireita from "../../assets/CirculeSetaDireita.svg";
@@ -15,203 +15,140 @@ import "./telaOng.css";
 function TelaOng() {
   return (
     <>
-    <NavInstitucional/>
-    <div className="sessao-um">
-      <session>
-        
-        <div class="bloco-um">
-          <h1>
-            Seja voluntário e ganhe selos <br />
-            de reconhecimento
-          </h1>
-          <div className="trecho-texto">
-          <span >
-            Aqui na HelpLine, <b>reconhecemos e valorizamos</b> as contribuições
-            <br /> dos nossos voluntários através de selos de reconhecimento.
-            <br /> Ao ganhar esses selos, os voluntários recebem{" "}
-            <b>reconhecimento</b>
-            <br /> público e inspiram outros a se juntarem a nós na causa da
-            <br /> solidariedade. Juntos, construímos uma rede de apoio onde
-            <br />
-            <b>cada ação é valorizada</b>.
-          </span>
-          </div>     
-          </div>  
-           <div className="img-bloco-um">
-          <img src={GroupSelo} alt="selos" />
+      <NavInstitucional />
+      <session className="view-80 font-poppins d-flex flex-center ">
+        <div className="d-flex justify-space-between flex-center section1-ong">
+          <div class="div-texto-ong flex-gap-24">
+            <h1 className="h1-home">
+              Seja voluntário e ganhe selos de reconhecimento
+            </h1>
+            <div>
+              <span className="texto-ong">
+                Aqui na HelpLine, <span className="destaque-span">reconhecemos e valorizamos </span> as contribuições
+                dos nossos voluntários através de selos de reconhecimento.
+                Ao ganhar esses selos, os voluntários recebem{" "}
+                <span className="destaque-span"> reconhecimento </span>
+                público e inspiram outros a se juntarem a nós na causa da
+                solidariedade. Juntos, construímos uma rede de apoio onde
+                <span className="destaque-span"> cada ação é valorizada</span>.
+              </span>
+            </div>
+          </div>
+          <div className="div-img1">
+            <img className="img-selo" src={GroupSelo} alt="selos" />
+          </div>
         </div>
-        
-      </session>
-      </div>
 
+      </session>
       <div className='bar-left'><img className='img-verde' src={imgBordaVerde} alt="" /></div>
+      <session className="session-view font-poppins">
+        <div className="bloco-dois session-view">
+          <h1 className="h1-home">Conheça novas ONGs</h1>
 
-      <div className="sessao-dois">
-      <session >
-        <div className="bloco-dois">
-          <h1 className="titulo">Conheça novas ONGs</h1>
-
-          <div className="cards">
-
-             <div className="img-setaEsquerdaDois">
-            <img src={setaEsquerda} alt="seta esquerda" />
-          </div>
-
-            <div class="card-um">
-
-              <div className="img-cardUm">
-                <img src={imgCardUm} alt="img card um" />
-              </div>
-
-              <h1>Bem da Madrugada</h1>
-
-              <div className="texto-cardUm">
-              <span>
-                Nosso objetivo é promover o <br/>
-                direito ao lazer e à infância <br/>
-                saudável...
-              </span>
+          <div className="div-card-ong">
+            <div class="card-ong">
+              <div>
+                <img className="img-card-ong" src={imgCardUm} alt="" />
               </div>
               
-              <div className="btn-conhecer">
-              <button className='font-league bold custom-button-ong'><Link to={'/register'} className='link-button-ong'>Conhecer</Link></button>
+              <div className="div-text-ong">
+              <span className="titulo-card-ong">Bem da Madrugada</span>
+                <span className="text-card">
+                  Nosso objetivo é promover o 
+                  direito ao lazer e à infância
+                  saudável...
+                </span>
               </div>
+                <button className='btn-conhecer-ong'><Link to={'/register'} className="link-button-home">Conhecer</Link></button>
             </div>
-
-            <div class="card-dois">
-
-              <div className="img-cardDois">
-                <img src={imgCardDois} alt="img card dois" />
-              </div>
-
-              <h1>Nós do Bem</h1>
-
-              <div className="texto-carddois">
-              <span >
-                Principal objetivo combater a <br/>fome e a insegurança <br/>
-                alimentar em comunidades...
-              </span>
-              </div>
-
-              <div className="btn-conhecerdois">
-              <button className='font-league bold custom-button-ong'><Link to={'/register'} className='link-button-ong'>Conhecer</Link></button>
-              </div>
-            </div>
-
-            <div class="card-tres">
-
-              <div className="img-cardTres">
-                <img src={imgCardTres} alt="img card tres" />
-              </div>
-
-              <h1>Instituto Vivereh</h1>
-
-              <div className="texto-cardtres">
-              <span className="">
-                Tem como missão fornecer <br/>
-                Roupas e itens essenciais para<br/>
-                indivíduos e famílias em...
-              </span>
+            <div class="card-ong">
+              <div>
+                <img className="img-card-ong" src={imgCardUm} alt="" />
               </div>
               
-              <div className="btn-conhecertres">
-              <button className='font-league bold custom-button-ong'><Link to={'/register'} className='link-button-ong'>Conhecer</Link></button>
+              <div className="div-text-ong">
+              <span className="titulo-card-ong">Bem da Madrugada</span>
+                <span className="text-card">
+                  Nosso objetivo é promover o 
+                  direito ao lazer e à infância
+                  saudável...
+                </span>
               </div>
-            </div>  
-
-              <div className="img-setaDireitaDois">
-            <img src={setaDireita} alt="seta direita" />
-          </div>
-
+                <button className='btn-conhecer-ong'><Link to={'/register'} className="link-button-home">Conhecer</Link></button>
+            </div>
+            <div class="card-ong">
+              <div>
+                <img className="img-card-ong" src={imgCardUm} alt="" />
+              </div>
+              
+              <div className="div-text-ong">
+              <span className="titulo-card-ong">Bem da Madrugada</span>
+                <span className="text-card">
+                  Nosso objetivo é promover o 
+                  direito ao lazer e à infância
+                  saudável
+                </span>
+              </div>
+                <button className='btn-conhecer-ong'><Link to={'/register'} className="link-button-home">Conhecer</Link></button>
+            </div>
           </div>
 
         </div>
       </session>
-      </div>
 
       <div className='bar-right'><img className='img-laranja' src={imgBordaLaranja} alt="" /></div>
-      
-      <div className="sessao-tres">
-      <session>
-        <div className="bloco-tres">
-        <h1 className="titulo">Vagas disponíveis</h1>
-       
-        <div className="img-setaEsquerdaTres">
-            <img src={setaEsquerda} alt="seta esquerda" />
-          </div>
-        <div className="cards">
-            <div class="card-um">
-              <div className="img-cardUm">
-                <img src={imgCardUm} alt="img card um" />
-              </div>
+        <session className="section-ong3 view-90 font-poppins">
+        <h1 className="h1-home">Vagas disponíveis</h1>
+          <div className="bloco-tres">
 
-              <div className="titulo-recepcionista">
-              <h1>Recepcionista</h1>
-              </div>
+            <div className="card-ong-session3">
+              <div class="card-um">
+                <div className="img-cardUm">
+                  <img className="img-card-ong" src={imgCardUm} alt="img card um" />
+                </div>
 
-              <div className="subtitulo-bemdamadrugada">
-              <b>
-              ONG: Bem da Madrugada
-              </b>
+                <div className="div-text-ong">
+                  <h4>Recepcionista</h4>
+                  <span className="text-card">
+                    ONG: Bem da Madrugada
+                </span>
+                <button className='btn-conhecer-ong'><Link to={'/institucional/volunteer'} className="link-button-home">Saiba mais</Link></button>
+                </div>
               </div>
-
-              <div className="btn-saiba">
-              <button className='font-league bold custom-button-ong'><Link to={'/institucional/volunteer'} className='link-button-ong'>Saiba mais</Link></button>
-              </div>
-
             </div>
-            <div class="card-dois">
+            <div className="card-ong-session3">
+              <div class="card-um">
+                <div className="img-cardUm">
+                  <img className="img-card-ong" src={imgCardUm} alt="img card um" />
+                </div>
 
-              <div className="img-cardDois">
-                <img src={imgCardDois} alt="img card dois" />
+                <div className="div-text-ong">
+                  <h4>Recepcionista</h4>
+                  <span className="text-card">
+                    ONG: Bem da Madrugada
+                </span>
+                <button className='btn-conhecer-ong'><Link to={'/institucional/volunteer'} className="link-button-home">Saiba mais</Link></button>
+                </div>
               </div>
-
-              <div className="titulo-cozinheiro">
-              <h1>Cozinheiro</h1>
-              </div>
-
-              <div className="subtitulo-nosdobem">
-              <b>
-              ONG: Nós do Bem
-              </b>
-              </div>
-
-              <div className="btn-saibadois">
-              <button className='font-league bold custom-button-ong'><Link to={'/institucional/volunteer'} className='link-button-ong'>Saiba mais</Link></button>
-              </div>
-
             </div>
-            <div class="card-tres">
+            <div className="card-ong-session3">
+              <div class="card-um">
+                <div className="img-cardUm">
+                  <img className="img-card-ong" src={imgCardUm} alt="img card um" />
+                </div>
 
-              <div className="img-cardTres">
-                <img src={imgCardTres} alt="img card tres" />
+                <div className="div-text-ong">
+                  <h4>Recepcionista</h4>
+                  <span className="text-card">
+                    ONG: Bem da Madrugada
+                </span>
+                <button className='btn-conhecer-ong'><Link to={'/institucional/volunteer'} className="link-button-home">Saiba mais</Link></button>
+                </div>
               </div>
-
-              <div className="titulo-juridico">
-              <h1>Jurídico</h1>
-              </div>
-
-              <div className="subtitulo-instituto">
-              <b>
-              ONG: Instituto Vivereh
-              </b>
-              </div>
-
-              <div className="btn-saibatres">
-              <button className='font-league bold custom-button-ong'><Link to={'/institucional/volunteer'} className='link-button-ong'>Saiba mais</Link></button>
-              </div>
-            </div> 
-
-           <div className="img-setaDireitaTres">
-            <img src={setaDireita} alt="seta direita" />
+            </div>
           </div>
-
-          </div>
-
-          </div>
-      </session>
-      <Footer />
-      </div>
+        </session>
+        <Footer />
     </>
   );
 }
